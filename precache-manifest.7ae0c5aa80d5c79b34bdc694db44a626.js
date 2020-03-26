66f6963426d94a3bb58991aa45e7e911afcaf46e
Anibal <anibal.eduardo@protonmail.ch>
2020-03-25 23:00:34 -0300

    Updates

---
+++ precache-manifest.7ae0c5aa80d5c79b34bdc694db44a626.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135932cfa95bf6f6e234b43f4245080a",
+    "url": "/NimrodHunter/natura-cura/index.html"
+  },
+  {
+    "revision": "6eae72366f786154bee1",
+    "url": "/NimrodHunter/natura-cura/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6200124166600eefb3b8",
+    "url": "/NimrodHunter/natura-cura/static/css/main.5495d3b4.chunk.css"
+  },
+  {
+    "revision": "6eae72366f786154bee1",
+    "url": "/NimrodHunter/natura-cura/static/js/2.6a9f2815.chunk.js"
+  },
+  {
+    "revision": "6200124166600eefb3b8",
+    "url": "/NimrodHunter/natura-cura/static/js/main.14cf1fd5.chunk.js"
+  },
+  {
+    "revision": "863b82dc1be47bfba4a4",
+    "url": "/NimrodHunter/natura-cura/static/js/runtime-main.96bbcd8a.js"
+  },
+  {
+    "revision": "37b7b07e163b0dc19fe6edf7687b16c2",
+    "url": "/NimrodHunter/natura-cura/static/media/alcon_montaña7colores.37b7b07e.png"
+  },
+  {
+    "revision": "282827fe0092765439a882df8d9fc4bc",
+    "url": "/NimrodHunter/natura-cura/static/media/atardecer_copacabana.282827fe.png"
+  },
+  {
+    "revision": "81a8a66a603b9ffc170e85dc4ad0f0e6",
+    "url": "/NimrodHunter/natura-cura/static/media/bahia_isla_del_sol.81a8a66a.png"
+  },
+  {
+    "revision": "e0f9135a1df137345731f25234a7cfae",
+    "url": "/NimrodHunter/natura-cura/static/media/certificado.e0f9135a.png"
+  },
+  {
+    "revision": "b954ce0cb4807da8dfe6883ef283f17f",
+    "url": "/NimrodHunter/natura-cura/static/media/machupichu.b954ce0c.png"
+  },
+  {
+    "revision": "224770815d4d2f4a134295037ce453a0",
+    "url": "/NimrodHunter/natura-cura/static/media/mariposa_copacabana.22477081.png"
+  },
+  {
+    "revision": "cbfb016df38e7dd777f0b1955188c7ad",
+    "url": "/NimrodHunter/natura-cura/static/media/programando.cbfb016d.jpeg"
+  },
+  {
+    "revision": "f3178c41c2624dd2ca82475cfbd58098",
+    "url": "/NimrodHunter/natura-cura/static/media/scout.f3178c41.png"
+  },
+  {
+    "revision": "d5e47ad93e7f0553c8c633c318aa32eb",
+    "url": "/NimrodHunter/natura-cura/static/media/sesion_kata_1.d5e47ad9.png"
+  },
+  {
+    "revision": "77c4330037e5579ef750eccafcae6701",
+    "url": "/NimrodHunter/natura-cura/static/media/sesion_kata_2.77c43300.png"
+  }
+]);
