040da0591b2ee2fca3ce105dcdd6f901c07c17b0
Anibal <anibal.eduardo@protonmail.ch>
2020-03-26 22:59:41 -0300

    header only styled-component & responsibe

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -16,86 +16,200 @@ interface INav {
   contact: string
 };
 
-const HeaderStyle = styled("header")`
-  height: 100px;
-  width: 100%;
-  background: transparent;
-  
-  font-family: futura-pt, sans-serif;
-  font-size: 15px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-weight: 500;
-  line-height: 1em;
+interface IMedia { 
+  name: string
+  url: string
+}
 
-  svg.bars-svg {
-    display: none;
-    fill: #fff;    
-  }
+const Header = () => {
+  const [ nav, ] = useState<INav>({
+    title: 'ANIBAL CATALÁN',
+    sections: ['yo', 'masajes'],
+    contact: 'contacto'
+  })
 
-  @media (max-width: 874px) {
-    .responsive-toolbar {
-      display: none;
-    }
+  const myRef = useRef(null);
+  const { width, } = useResize(myRef)
+
+  const [ navOpen, setNavOpen ] = useState(false);
+
+  const HeaderStyle = styled("header")`
+    height: 10vh;
+    width: 100vW;
+    background: transparent;
 
     svg.bars-svg {
       display: block;
+      fill: #fff;    
     }
 
-    svg.close-svg {
-      fill: #fff;
-    }
-
-  }
-
-  .title {
-    font-size: 30px;
-    color: #fff;
-    display: flex;
-    margin-block-start: 0;
-    margin-block-end: 0;
-    padding-inline-start: 2%;
-    height: 100%;
-    align-items: center;
-    float: left;
-
-    a {
-      color: #fff;
-      padding: 10px 10px 10px 10px;
-    }
-
-    :hover {
-      color: #fff;
-    }
-
-    :focus {
-      color: #fff;
-      outline: none;
-    }
-  }
-
-  .responsive-toolbar {
-    height: 100%;
-    align-items: center;
-    width: 100%;
-
-    ul {
-      display: flex;
-      margin-block-start: 0;
-      margin-block-end: 0;
-      padding-inline-start: 0;
-      height: 100%;
-      align-items: center;
-      float: right;
-
-      li {
-        list-style-type: none;
-        margin: 20px 20px;
+    @media (min-width: 770px) {
+      .responsive-toolbar {
+        display: block;
       }
+
+      svg.bars-svg {
+        display: none;
+      }
+
+      svg.close-svg {
+        fill: #fff;
+      }
+
+    }
+
+    .title {
+      height: 100%;
+      font-family: 'Lato', sans-serif;
+      font-size: 1rem;
+      letter-spacing: 0.1rem;
+      color: #fff;
+      display: flex;
+      padding-inline-start: 5%;
+      float: left;
+      align-items: center;
 
       a {
         color: #fff;
         padding: 10px 10px 10px 10px;
+      }
+
+      :hover {
+        color: #fff;
+      }
+
+      :focus {
+        color: #fff;
+        outline: none;
+      }
+    }
+
+    .openBars {
+      display: ${width <= 770 && !navOpen ? 'block' : 'none'};
+      transition: display 200ms ease;
+      padding: 0;
+      padding-inline-end: 3%;
+      height: 100%;
+      float: right;
+      background: transparent;
+      border: none;
+    }
+
+    .menu {
+      display: ${width >= 770 ? 'block' : 'none'};
+      transition: display 200ms ease;
+      height: 100%;
+      width: 100%;
+      align-items: center;
+
+      font-family: 'Lato', sans-serif;
+      font-size: 0.8rem;
+      text-transform: uppercase;
+      letter-spacing: 0.08rem;
+      padding-inline-end: 5%;
+
+      ul {
+        display: flex;
+        margin-block-start: 0;
+        margin-block-end: 0;
+        padding-inline-start: 0;
+        height: 100%;
+        align-items: center;
+        float: right;
+
+        li {
+          list-style-type: none;
+          margin: 0 0 0 1rem;
+        }
+
+        a {
+          color: #fff;
+          padding: 10px 10px 10px 10px;
+
+          :hover {
+            color: #9c9999;
+          }
+
+          :focus {
+            color: #9c9999;
+            outline: thin dotted #fff;
+          }
+        }
+
+        .contact {
+          border: 2px solid #fff;
+          border-spacing: 15px;
+
+          :hover {
+            color: #181818;
+            background: #fff;
+          }
+
+          :focus {
+            background: #fff;
+            color: #181818;
+            outline: none;
+          }
+        }
+      }
+    }
+  `;
+  
+  const SideBarStyle = styled("div")`
+    display: ${width < 770 && navOpen ? 'block' : 'none'};
+    transition: display 200ms ease;
+    overflow-y: hidden;
+    height: 100vh;
+    width: 35vw;  
+    float: right;
+    background: #201a16;
+    z-index: 200;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    text-align: center;
+
+    font-family: 'Lato', sans-serif;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    letter-spacing: 0.05rem;
+
+    .closeCross {
+      display: ${width < 770 && navOpen ? 'block' : 'none'};
+      transition: display 200ms ease;
+      position: fixed;
+      right: 34vw;
+      float: right;
+      background: transparent;
+      border: none;
+      fill: #fff;
+      padding: 0;
+      height: 10vh;
+    }
+    
+    svg.email-svg, svg.instagram-svg, svg.whatsapp-svg {
+      margin: 10px 10px 0px 10px;
+      fill: rgba(255,255,255,.4)
+    }
+
+    svg.email-svg:hover, svg.instagram-svg:hover, svg.whatsapp-svg:hover {
+      fill: #fff;
+    }
+
+    ul {
+      padding-top: 3.3vh;
+      margin-block-start: 0;
+      padding-inline-start: 0;
+
+      li {
+        list-style-type: none;
+        margin: 0 10px 1.2rem 10px;
+      }
+
+      a {
+        color: #fff;
+        padding: 10px;
 
         :hover {
           color: #9c9999;
@@ -123,127 +237,7 @@ const HeaderStyle = styled("header")`
         }
       }
     }
-  }
-
-`;
-
-const SideBarStyle = styled("div")`
-  overflow: hidden;
-  height: 100%;
-  width: 200px;  
-  float: right;
-  background: #201a16;
-  padding: 20px 25px 100% 25px;
-  z-index: 200;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;  
-
-  font-family: futura-pt, sans-serif;
-  font-size: 15px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-weight: 500;
-  line-height: 2.5em;
-
-  svg.email-svg, svg.instagram-svg, svg.whatsapp-svg {
-    margin: 0px 10px 0px 10px;
-    fill: rgba(255,255,255,.4)
-  }
-
-  svg.email-svg:hover, svg.instagram-svg:hover, svg.whatsapp-svg:hover {
-    fill: #fff;
-  }
-
-  ul {
-    margin-block-start: 0;
-    margin-block-end: 0;
-    padding-inline-start: 0;
-    align-items: center;
-
-    li {
-      list-style-type: none;
-      margin: 20px 20px;
-    }
-
-    a {
-      color: #fff;
-      padding: 10px 10px 10px 10px;
-
-      :hover {
-        color: #9c9999;
-      }
-
-      :focus {
-        color: #9c9999;
-        outline: thin dotted #fff;
-      }
-    }
-
-    .contact {
-      border: 2px solid #fff;
-      border-spacing: 15px;
-
-      :hover {
-        color: #181818;
-        background: #fff;
-      }
-
-      :focus {
-        background: #fff;
-        color: #181818;
-        outline: none;
-      }
-    }
-  }
-`;
-
-const Header = () => {
-  const [ nav, ] = useState<INav>({
-    title: 'ANIBAL CATALÁN',
-    sections: ['yo', 'masajes'],
-    contact: 'contacto'
-  })
-
-  const myRef = useRef(null);
-  const { width, } = useResize(myRef)
-
-  const [ navOpen, setNavOpen ] = useState(false)
-
-  const toggleBars = {
-    display: `${width <= 860 && !navOpen ? 'block' : 'none'}`,
-    float: 'right' as 'right',
-    background: 'transparent',
-    color: 'transparent',
-    border: 'none',
-    padding: '0px 2% 0px 0px',
-    msTransform: 'translateY(100%)',
-    transform: 'translateY(100%)'
-  }
-
-  const toggleClose = {
-    display: `${width < 860 && navOpen ? 'block' : 'none'}`,
-    position: 'fixed' as 'fixed',
-    right: '200px',
-    float: 'right' as 'right',
-    background: 'transparent',
-    color: 'transparent',
-    border: 'none',
-    padding: '0px 2% 0px 0px',
-    msTransform: 'translateY(100%)',
-    transform: 'translateY(100%)',
-  }
-
-  const sideBars = {
-    display: `${width < 860 && navOpen ? '' : 'none'}`,
-    transition: 'display 0s linear 0.5s',
-  }
-
-  interface IMedia { 
-    name: string
-    url: string
-  }
+  `;
   
   const [media, ] = useState<Array<IMedia>>(
     [
@@ -253,40 +247,35 @@ const Header = () => {
     ]
   )
 
-  const Social = {
-    textAlign: 'center' as 'center',
-    padding: '10px 0px 0 0px',
-  }
-
   return (
     <React.Fragment>
-      <SideBarStyle ref={ myRef } style={ sideBars }>
-      <ul>
-        {nav.sections.map(section => {
-          return (
-            <li key={ section }><Link to ={ `/${ section }` } key={ section }>{ section.toUpperCase() }</Link></li>
-          )
-        })}
-        <li key={ nav.contact }><Link to ={ `/${ nav.contact }` } key={ nav.contact } className="contact">{ nav.contact.toUpperCase() }</Link></li>
-      </ul>
-      <div style={ Social }>
-        <a href={ media[0].url } className={ media[0].name }><WhatsAppSVG width="20%" height="20%"/></a>
-        <a href={ media[1].url } className={ media[1].name }><InstagramSVG width="20%" height="20%"/></a>
-        <a href={ media[2].url } className={ media[2].name }><EmailSVG width="20%" height="20%"/></a>
-      </div>
+      <SideBarStyle ref={ myRef } >
+        <button className="closeCross" onClick = { () => setNavOpen(!navOpen) }><CloseSVG width="33.33px" height="22.227px" /></button>
+        <ul>
+          {nav.sections.map(section => {
+            return (
+              <li key={ section }><Link to ={ `/${ section }` } key={ section } onClick = { () => setNavOpen(!navOpen) } >{ section.toUpperCase() }</Link></li>
+            )
+          })}
+          <li key={ nav.contact }><Link to ={ `/${ nav.contact }` } key={ nav.contact } className="contact"  onClick = { () => setNavOpen(!navOpen) }>{ nav.contact.toUpperCase() }</Link></li>
+        </ul>
+        <div>
+          <a href={ media[0].url } className={ media[0].name } ><WhatsAppSVG width="15%" height="15%"/></a>
+          <a href={ media[1].url } className={ media[1].name } ><InstagramSVG width="15%" height="15%"/></a>
+          <a href={ media[2].url } className={ media[2].name } ><EmailSVG width="15%" height="15%"/></a>
+        </div>
       </SideBarStyle>
       <HeaderStyle ref={ myRef }>
         <Link to ={ `/` } key={ nav.title } className="title">{ nav.title.toUpperCase() }</Link>
-        <button style= { toggleBars } onClick = { () => setNavOpen(!navOpen) }><BarsSVG width="50px" height="33.34px" /></button>
-        <button style= { toggleClose } onClick = { () => setNavOpen(!navOpen) }><CloseSVG width="50px" height="33.34px" /></button>
-        <nav className="responsive-toolbar">
+        <button className="openBars" onClick = { () => setNavOpen(!navOpen) }><BarsSVG width="50px" height="33.34px" /></button>
+        <nav className="menu">
           <ul>      
             {nav.sections.map(section => {
               return (
                 <li key={ section }><Link to ={ `/${ section }` } key={ section }>{ section.toUpperCase() }</Link></li>
               )
             })}
-            <li key={ nav.contact }><Link to ={ `/${ nav.contact }` } key={ nav.contact } className="contact">{ nav.contact.toUpperCase() }</Link></li>
+            <li key={ nav.contact } ><Link to ={ `/${ nav.contact }` } key={ nav.contact } className="contact">{ nav.contact.toUpperCase() }</Link></li>
           </ul> 
         </nav>
       </HeaderStyle > 
